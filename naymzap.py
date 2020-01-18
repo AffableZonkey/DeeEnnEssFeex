0832ef69fffe68dc9ad2fbce810f2ec987371487
XakV <zachvatwork@gmail.com>
2020-01-17 23:29:17 -0500

    changed name switched from click to typer refactoring

--- naymzap.py
+++ naymzap.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import typer
+import sys
 
 naymzap = typer.Typer()
 
@@ -32,20 +33,34 @@ def get_gandi_key():
 @naymzap.command()
 def get_zone_info(zone: str):
     zone_headers = {"X-Api-Key": get_gandi_key()}
-    zone_info_get = requests.get(('https://dns.api.gandi.net/api/v5/domains/' + zone + '/records'),
-                                 headers=zone_headers)
-    zone_json = zone_info_get.json()
-    for item in zone_json:
-        typer.echo(json.dumps(item))
+    try:
+        zone_info_get = requests.get(('https://dns.api.gandi.net/api/v5/domains/' + zone + '/records'),
+                                     headers=zone_headers)
+    except requests.exceptions.RequestException as e:
+        typer.echo(e, f'Naymzap Failed: {} ')
+        sys.exit(1)
+    if zone_info_get.status_code == 200:
+        return zone_info_get.json()
+    else:
+        typer.echo('Not my fault, the internet fell over.')
+#   for item in zone_json:
+#       typer.echo(json.dumps(item))
 
 
 @naymzap.command()
 def get_record(zone: str, dom_rec_name: str):
     zone_headers = {"X-Api-Key": get_gandi_key()}
     zone_parms = {"rrset_type": dom_rec_name}
-    zone_rec_get = requests.get(('https://dns.api.gandi.net/api/v5/domains/' + zone + '/records/' + dom_rec_name),
-                                headers=zone_headers, params=zone_parms)
-    typer.echo(zone_rec_get.json())
+    try:
+        zone_rec_get = requests.get(('https://dns.api.gandi.net/api/v5/domains/' + zone + '/records/' + dom_rec_name),
+                                    headers=zone_headers, params=zone_parms)
+    except requests.exceptions.RequestException as e:
+        typer.echo(e, f'Naymzap Failed: {} ')
+        sys.exit(1)
+    if zone_rec_get.status_code == 200:
+        return zone_rec_get.json()
+    else:
+        typer.echo('Not my fault, there was a hole in the bits bucket.')
 
 
 @naymzap.command()
@@ -60,6 +75,18 @@ def update_record(zone: str, dom_rec_name: str, dom_rec_type: str, rec_new_val: 
         headers=zone_headers, json=zone_data)
     typer.echo(zone_rec_chg.json())
 
+@naymzap.command()
+def auto_dns_fix(domain: str,):
+    zone_records = get_zone_info(domain)
+    ptr, ip_addr = get_ip()
+    for record in zone_records:
+        if record.rrset_type and record.rrset_type == "A":
+            if ip_addr not in record.rrset_values.iteritems() and len(record.rrset_values) <= 1:
+                update_record(domain, record.dom_rec_nam, dom_rec_type, ip_addr)
+            else:
+                typer.echo('I oopsied')
+        else:
+            continue
 
 if __name__ == "__main__":
     naymzap()
